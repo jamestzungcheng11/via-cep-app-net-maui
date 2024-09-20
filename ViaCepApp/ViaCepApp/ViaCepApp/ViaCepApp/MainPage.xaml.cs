6f032034a732339ef8e35929ee66bbcd6f380b08
jamestzungchengguo <jamestzungcheng11@gmail.com>
2024-09-20 11:42:33 -0300

    first commit

---
+++ ViaCepApp/ViaCepApp/ViaCepApp/ViaCepApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Newtonsoft.Json.Linq;
+
+namespace ViaCepApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnConsultarClicked(object sender, EventArgs e)
+        {
+            string cep = cepEntry.Text?.Trim();
+
+            if (string.IsNullOrEmpty(cep) || cep.Length != 8)
+            {
+                resultadoLabel.Text = "Digite um CEP válido com 8 dígitos.";
+                return;
+            }
+
+            try
+            {
+                string url = $"https://viacep.com.br/ws/{cep}/json/";
+                string response = await httpClient.GetStringAsync(url);
+
+                var endereco = JObject.Parse(response);
+
+                if (endereco["erro"] != null)
+                {
+                    resultadoLabel.Text = "CEP não encontrado.";
+                }
+                else
+                {
+                    resultadoLabel.Text = $"Endereço: {endereco["logradouro"]}, {endereco["bairro"]}, {endereco["localidade"]} - {endereco["uf"]}";
+                }
+            }
+            catch (Exception ex)
+            {
+                resultadoLabel.Text = $"Erro: {ex.Message}";
+            }
+        }
+    }
+}
+
---
+++ ViaCepApp/ViaCepApp/ViaCepApp/ViaCepApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ViaCepApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
